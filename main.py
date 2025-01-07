95f946c60d21eb3e0cc1911d97f67e30dabf3dd0
LourMuna <lourmuna2019@gmail.com>
2025-01-07 20:11:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 
+%load_ext autoreload%autoreload 2# Download the data from your GitHub repository!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+import pandas as pd
+df = pd.read_csv('/content/parkinsons.csv')
+df = df.dropna()
+import seaborn as sns
+sns.pairplot(df, hue= "status"  )
+selected_feutures = ['spread1', 'PPE']
+target = "status"
+x = df[selected_feutures]
+y = df[target]
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+x_scaled = scaler.fit_transform(x)
+x_scaled = pd.DataFrame(x_scaled, columns=x.columns)
+from sklearn.model_selection import train_test_split
+x_train, x_val, y_train, y_val = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
+from sklearn.tree import DecisionTreeClassifier
+knn_model = DecisionTreeClassifier(max_depth = 3 )
+knn_model.fit(x_train, y_train)
+from sklearn.metrics import accuracy_score
+y_pred = knn_model.predict(x_val)
+accuracy = accuracy_score(y_val, y_pred)
+print(f"Accuracy on the test set: {accuracy}")
